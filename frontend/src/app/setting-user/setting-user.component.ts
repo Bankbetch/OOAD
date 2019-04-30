720441d54144516e26a9a047c0a27b2ebb238396
bank <jiraphun.jrb@gmail.com>
2019-04-30 21:51:57 +0700

    fix

--- frontend/src/app/setting-user/setting-user.component.ts
+++ frontend/src/app/setting-user/setting-user.component.ts
@@ -140,7 +140,7 @@ export class SettingUserComponent implements OnInit {
         this.allowAlertInsertFail = true
         setTimeout(() => {
           this.allowAlertInsertFail = false
-        }, 5000);
+        }, 3000);
       }
       this.onGetTable()
       this.OnClear()
