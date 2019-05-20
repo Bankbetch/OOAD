20e86a40fb8c8cbccac431e687112c7eb56718e6
TAOCHIZ <42669047+taochiz@users.noreply.github.com>
2019-05-20 23:26:53 +0700

    เพิ่มอาจารย์

---
+++ frontend/src/app/teacher/teacher.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Title } from "@angular/platform-browser";
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@Component({
+  selector: 'app-teacher',
+  templateUrl: './teacher.component.html',
+  styleUrls: ['./teacher.component.css']
+})
+export class TeacherComponent implements OnInit {
+
+  constructor(private http: HttpClient,
+    private title: Title,
+    private spinner: NgxSpinnerService) {
+    this.title.setTitle("ตารางสอบ")
+  }
+  getName
+  getSurname
+  dataShow = []
+  dataNisit = []
+
+  ngOnInit() {
+    this.getName = localStorage.getItem("getName")
+    this.getSurname = localStorage.getItem("getSurname"), this.getSchedule()
+  }
+
+  getSchedule() {
+    this.http.get<any>('http://localhost:4001/exam').subscribe(result => {
+      var dataExams = []
+      for (var item of result.data) {
+        if (item.statusExam == "เปิดการสอบ") {
+          dataExams.push(item)
+        }
+      }
+      for (var item of dataExams) {
+        for (var item2 of item.teacher) {
+          var splitted = item2.split(" ");
+          var name = splitted[0]
+          var surname = splitted[1]
+          if (name === this.getName && surname === this.getSurname) {
+            this.dataShow.push({
+              id: item.id, name: item.name,
+              room: item.room, day: item.day, faculty: item.faculty,
+              time: item.timeStart + " - " + item.timeEnd,
+              sit: item.examSit
+            })
+            console.log(this.dataShow)
+          }
+        }
+      }
+    })
+  }
+
+
+}
